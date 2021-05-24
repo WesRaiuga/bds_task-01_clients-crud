bcc650fb521aa01aff17fc2ed7f2703902b42316
WesRaiuga <weslley_wenny@hotmail.com>
2021-05-24 17:06:54 -0300

    Project created

---
+++ src/main/java/br/dev/wesraiuga/bds/bds_task01_clientscrud/BdsTask01ClientsCrudApplication.java
@@ -0,0 +1,13 @@
+package br.dev.wesraiuga.bds.bds_task01_clientscrud;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BdsTask01ClientsCrudApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BdsTask01ClientsCrudApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/dev/wesraiuga/bds/bds_task01_clientscrud/BdsTask01ClientsCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package br.dev.wesraiuga.bds.bds_task01_clientscrud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BdsTask01ClientsCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
